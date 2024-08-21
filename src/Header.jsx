42fb7a74d91a3152fcdc0fb74a34ddab3ed6909b
Eraj Tanweer <erajtanweer2@gmail.com>
2024-08-21 21:20:14 +0500

    logo-set

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@
 function Header() {
   return (
     <div className='header'>
-      <img src="./PIHRM-logo.png" alt="no logo found" />
+      <img src="../PIHRM-logo.png" alt="no logo found" />
       <div className='header-headings'>
         <div><h5>About Us</h5></div>
         <div><h5>Members</h5></div>
