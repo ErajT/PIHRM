ad1476d664697b6f48dc924164fa2bdaf048b6a2
Eraj Tanweer <erajtanweer2@gmail.com>
2024-08-21 23:32:42 +0500

    hi

--- src/HeaderTop.jsx
+++ src/HeaderTop.jsx
@@ -1,11 +1,35 @@
-// import React from 'react'
+// import React from 'react';
 
 const HeaderTop = () => {
   return (
-    <div id='HeaderTop'>
-        <img src="./image.jpg" alt="no logo present" />
+    <div id="HeaderTop" style={headerTopStyle}>
+      <div style={imgContainerStyle}>
+        <img src="./image.jpg" alt="no logo present" style={imgStyle} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeaderTop
+// Inline styles for the div and img
+const headerTopStyle = {
+  height: '7vh',
+  backgroundColor: 'rgb(188, 212, 231)',
+  display: 'flex',
+  alignItems: 'center', // Center the content vertically
+  position: 'relative', // Ensure absolute positioning works correctly
+};
+
+const imgContainerStyle = {
+  flex: '1',
+  display: 'flex',
+  justifyContent: 'center', // Center the image horizontally
+};
+
+const imgStyle = {
+  maxWidth: '100%',
+  maxHeight: '100%',
+  width: 'auto',
+  height: 'auto',
+};
+
+export default HeaderTop;
